8a2f09b9e979ca8369ef0517303dfe34f1aefdc6
Bramandityo Prabowo <freez_meinster@yahoo.co.id>
2011-02-28 20:54:56 +0700

    merubah setting

--- belajar/models.py
+++ belajar/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class MataPelajaran(models.Model):
+	nama = models.CharField(max_length=50)
+
+class MateriPelajaran(models.Model):
+	matapelajaran = models.ForeignKey('MataPelajaran')
+	judul = models.CharField(max_length=255)
+	kontent = models.TextField()
+	tgl_post = models.DateField()
+	penulis = models.ForeignKey('User')
