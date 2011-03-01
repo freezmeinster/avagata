e8571e7dd6580680ccaab97af6ce3e7110339e9c
Bramandityo Prabowo <freez_meinster@yahoo.co.id>
2011-03-02 00:32:07 +0700

    masukin static content

--- urls.py
+++ urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+import os.path
 
 admin.autodiscover()
+asset = os.path.join(os.path.dirname(__file__),'asset')
 
 urlpatterns = patterns('',
     # Example:
     # (r'^avagata/', include('avagata.foo.urls')),
 	(r'^admin/', include(admin.site.urls)),
+	(r'^asset/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': asset}),
+
 )
